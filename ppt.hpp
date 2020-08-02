0be1e7d77f8facfb59c665c5ec23172727b2b9e8
Guillermo <Guillermo.pitagil@gmail.com>
2020-08-02 17:00:13 +0200

    Refactor with hpp and ppt.cpp

---
+++ ppt.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+// Rock Paper Sscissors game
+std::string random_ppt (void);
+int who_won(std::string user_choice,std::string machine_choice);
+void output_result_current (int winner_current);
+std::string choice_machine_toString(std::string machine_choice);
+
+
+
+
