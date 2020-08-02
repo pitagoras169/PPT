963c6e1d7cca46aa99ba6a2b5dbd8a8787ea6551
Guillermo <Guillermo.pitagil@gmail.com>
2020-08-02 16:45:55 +0200

    added loop to keep playing

--- main.cpp
+++ main.cpp
@@ -99,33 +99,59 @@ void output_result_current (int winner_current){
 	}
 }
 
+std::string choice_machine_toString(std::string machine_choice){
+	if (machine_choice == "r"){
+		return "Rock";
+	} else if (machine_choice == "p"){
+		return "Paper";
+	} else if (machine_choice == "s"){
+		return "Scissors";
+	} else{
+		return "Error";
+	}
+}
+
 
 int main(void){
 
 	int nb_partidas = 0;
+	int iter;
 	std::string user_choice;
 	std::string machine_choice;
+	std::string machine_choice_msg;
 	int winner_current;
+	int max_nb_games = 100;
 
 
 	// Mensaje de bienvenida
 	std::cout << "Wellcome to Rock Paper Scissors. \n";
 
+	for (iter = 0 ; iter < max_nb_games ; iter++){
+		//Next user move message
+		std::cout << "What is your next move ? Rock (r), Paper (p) or Scissors (s) ? Quit (q) ";
+		std::cin >> user_choice;
 
-	//Next user move message
-	std::cout << "What is your next move ? Rock (r), Paper (p) or Scissors (s) ? ";
-	std::cin >> user_choice;
+		if(user_choice == "q"){
+			break;
+		}
 
-	//Next Machine move
-	machine_choice = random_ppt();
-	std::cout << "Machine Choice = " + machine_choice + "\n";
+		//Next Machine move
+		machine_choice = random_ppt();
+		machine_choice_msg = choice_machine_toString(machine_choice);
+		std::cout << "Machine Choice = " + machine_choice_msg + "\n";
 
-	//Calculate who wins
-	winner_current = who_won(user_choice,machine_choice);
+		//Calculate who wins
+		winner_current = who_won(user_choice,machine_choice);
 
-	//Output the result
-	output_result_current(winner_current);
+		//Output the result
+		output_result_current(winner_current);
+	}
 
+	
+
+
+	//Exit message
+	std::cout << "Thanks for playing with me. Hope to see you soon :-)\n";
 
 
 
